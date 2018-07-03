e7a9c38ab05d0463f1be79b8c51c7728b17cbf1c
Charles Kolstad <charlespkolstad@gmail.com>
2018-07-03 06:24:24 -0600

    initial

---
+++ GoDaddyEmail/GoDaddyEmail_Model/ContactModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoDaddyEmail_Model
+{
+    public class ContactModel
+    {
+        public string EmailTo { get; set; }
+        public string ContactName { get; set; }
+        public string EmailBody { get; set; }
+    }
+}
---
+++ GoDaddyEmail/GoDaddyEmail_Web/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using GoDaddyEmail_Core;
+using GoDaddyEmail_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoDaddyEmail_Web.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index(ContactModel model)
+        {
+            string errorMessage = string.Empty;
+            EmailManager.SendEmail(model, out errorMessage);
+
+            if (string.IsNullOrEmpty(errorMessage) == false)
+                ViewBag.ErrorMessage = errorMessage;
+
+            return View();
+        }
+    }
+}
